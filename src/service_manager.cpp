8cb3af041a813da4adec03534ceb1796f2afe2d0
Markus Klemm <markus@markusklemm.net>
2016-01-19 14:30:40 +0100

    Choose the local adapter as expected

--- src/service_manager.cpp
+++ src/service_manager.cpp
@@ -3,14 +3,15 @@
 
 #include "service_manager.hpp"
 
-
+#include "local_adapter.hpp"
 
 namespace portable_service {
 
 service_manager::service_manager(const service_api &api) {
     switch (api) {
         case service_api::local:
-
+            this->adapter(internal::local_adapter());
+            break;
         case service_api::automatic:
         default:
             throw std::runtime_error("Not implemented yet");
